9a6e95596d883128986de3bb1a0008c4e0faeb34
xekvins <xekvins@icloud.com>
2024-01-21 18:11:13 +0100

    hm8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
+import javax.naming.ldap.PagedResultsResponseControl;
 import java.util.Arrays;
-
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Task 1
@@ -8,63 +9,73 @@ public class Main {
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        System.out.println(number[0]);
-        System.out.println(number[1]);
-        System.out.println(number[2]);
+
 
         //
         double [] numberSecond = {1.57, 7.654, 9.986};
-        System.out.println(numberSecond[0]);
-        System.out.println(numberSecond[1]);
-        System.out.println(numberSecond[2]);
 
 
         //
         int [] numberThird = new int[12];
         numberThird[0] = 938;
-        for (int i = 0; i < 12; i++) {
-            System.out.println(numberThird[i]);
-
+        for (int i = 0; i < numberThird.length; i++) {
 
         }
+        System.out.println(Arrays.toString(numberThird));
 
+
+        System.out.println();
         //Task 2
         System.out.println("Task 2");
-        System.out.println(number[0]);
-        System.out.println(number[1]);
-        System.out.println(number[2]);
+        for (int i = 0; i < number.length; i++) {
+        }
+        System.out.println(Arrays.toString(number));
 
 
-        System.out.println(numberSecond[0]);
-        System.out.println(numberSecond[1]);
-        System.out.println(numberSecond[2]);
+        for (int i = 0; i < numberSecond.length; i++) {
 
-
+        }
+        System.out.println(Arrays.toString(numberSecond));
 
 
         numberThird[0] = 938;
         for (int i = 0; i < 12; i++) {
-            System.out.println(numberThird[i]);
 
 
         }
+        System.out.println(Arrays.toString(numberThird));
+
+        System.out.println();
 
         //Task 3
         System.out.println("Task 3");
-        System.out.println(number[2]);
-        System.out.println(number[1]);
-        System.out.println(number[0]);
+        for (int i = number.length - 1; i >= 0; i--) {
+            System.out.println(number[i]);
+        }
+        System.out.println();
 
 
 
-        System.out.println(numberSecond[2]);
-        System.out.println(numberSecond[1]);
-        System.out.println(numberSecond[0]);
 
+        for (int i = numberSecond.length - 1; i >=0; i--) {
+            System.out.println(numberSecond[i]);
+        }
+        System.out.println();
 
         //Task 4
         System.out.println("Task 4");
-        int [] arr = {2, 4, 6, 8, 10, 12, 14};
-        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] % 2 != 0 ) {
+                number[i]++;
+                System.out.println(number[i]);
+
+            }
+        }
+
+
+            System.out.println(number[1]);
+
+
+
     }
 }
