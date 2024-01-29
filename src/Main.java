adfd69eb965a0f94eb394f029199f9f308f5458d
xekvins <xekvins@icloud.com>
2024-01-29 23:07:30 +0100

    hm9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+   //Task 1
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
+
+
+        //Task 2
+        System.out.println("Task 2");
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName);
+
+
+        //Task 3
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника — " + fullName);
     }
 }
