1de6a77be6bdbf42b52170c4d3e874637e25fa6f
xekvins <xekvins@icloud.com>
2024-02-02 03:32:56 +0100

    ewe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String nameJohn = "John";
+        int ageJohn = 13;
+
+        String nameSarah = "Sarah";
+        int ageSarah = 30;
+
+        String[] names = {"John", "Sarah"};
+        int[] ages = {13, 30};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Name - " + names[i] + " age - " + ages[i]);
+        }
+        Person sarah = new Person("Sarah", 30);
+        System.out.println("sarah.name = " + sarah.getName());
+        System.out.println("sarah.age = " + sarah.getAge());
+        sarah.setAge(31);
+        System.out.println("sarah.getAge() = " + sarah.getAge());
+        Person john = new Person("John", 13);
+
     }
 }
